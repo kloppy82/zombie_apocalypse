04e3b5af08f128e9a3b58bd32517378b2ef84f88
Markus Lempen <markus.lempen@damedics.com>
2018-07-05 23:20:13 +0200

    Initial Commit

---
+++ devices/index.js
@@ -0,0 +1,8 @@
+const ZombieBuzzer = require('./ZombieBuzzer');
+
+// export the devices you want to make available to the Brain (see README for more information)
+module.exports = {
+  devices: [
+    ZombieBuzzer
+  ],
+};
