b110a014e4cb7092bd63f52a40cd841a627c7559
Markus Lempen <markus.lempen@damedics.com>
2018-07-06 11:41:42 +0200

    sdfsdfgfg

--- devices/ZombieLED/controller.js
+++ devices/ZombieLED/controller.js
@@ -34,13 +34,15 @@ pyshell.on('message', function (message) {
 });
 
 // end the input stream and allow the process to exit
-pyshell.end(function (err,code,signal) {
-  if (err) throw err;
-  console.log('The exit code was: ' + code);
-  console.log('The exit signal was: ' + signal);
-  console.log('finished');
-});
 
+function endPython(){
+  pyshell.end(function (err,code,signal) {
+    if (err) throw err;
+    console.log('The exit code was: ' + code);
+    console.log('The exit signal was: ' + signal);
+    console.log('finished');
+  });
+}
 
 /**
  * One button handler for each registered button
