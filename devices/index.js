0451fc2dd4d78d8fb8ca089d672e4bdf74a8862d
Markus Lempen <markus.lempen@damedics.com>
2018-07-06 12:12:05 +0200

    asdfasdf

--- devices/index.js
+++ devices/index.js
@@ -4,7 +4,7 @@ const ZombieLED = require('./ZombieLED');
 // export the devices you want to make available to the Brain (see README for more information)
 module.exports = {
   devices: [
-  //  ZombieBuzzer,
+ 	ZombieBuzzer,
     ZombieLED
   ],
 };
