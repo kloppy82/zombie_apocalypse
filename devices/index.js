8c62c3ca64d41464963f8b4b0f14a81b86f77b0c
Markus Lempen <markus.lempen@damedics.com>
2018-07-06 12:20:28 +0200

    erwer

--- devices/index.js
+++ devices/index.js
@@ -1,4 +1,4 @@
-//const ZombieBuzzer = require('./ZombieBuzzer');
+const ZombieBuzzer = require('./ZombieBuzzer');
 const ZombieLED = require('./ZombieLED');
 
 // export the devices you want to make available to the Brain (see README for more information)
